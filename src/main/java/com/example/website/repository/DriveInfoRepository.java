7d7dbe2c176183a897cae6eec250f66c0ef9764e
khan Tanvir Ferdous <168400554+tanvir1811@users.noreply.github.com>
2025-03-27 19:10:00 +0600

    Update DriveInfoRepository.java

--- src/main/java/com/example/website/repository/DriveInfoRepository.java
+++ src/main/java/com/example/website/repository/DriveInfoRepository.java
@@ -2,11 +2,11 @@ package com.example.website.repository;
 
 import com.example.website.model.DriveInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface DriveInfoRepository extends JpaRepository<DriveInfo, Long> {
-    Optional<DriveInfo> findByUniqueId(String uniqueId); // Find a record by uniqueId
+
+    // Fetch drive info by uniqueId
+    Optional<DriveInfo> findByUniqueId(String uniqueId);
 }
